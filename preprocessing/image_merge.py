fda3b50cf6d630c91ac3cb9dc748000b4d5ec143
z520520115 <z520520115@gmail.com>
2022-07-07 20:04:29 +0900

    update

--- preprocessing/image_merge.py
+++ preprocessing/image_merge.py
@@ -1,37 +1,52 @@
 import cv2
 import os
 
-# video_path = r'C:/Users/YIHANG/PycharmProjects/HTAD/dataset/video_frames/'
-#
-# # 计算共有多少图片
-# count = 0
-# video_frames = []
-# for root, dirs, files in os.walk(video_path, topdown=True):
-#     for i in dirs:
-#         video_frames.append(root + i + "/")
-#
-#     for file in files:
-#         ext = os.path.splitext(file)[-1].lower()
-#         if ext == '.jps':
-#             count += 1
-#             print(count)
+video_frames_path = r'C:/Users/YIHANG/PycharmProjects/HTAD_dataset/video_frames/'
 
-image = cv2.imread('C:/Users/YIHANG/PycharmProjects/HTAD/dataset/video_frames/000049/19.jpg')
-size = (image.shape[1], image.shape[0])
-videowrite = cv2.VideoWriter(r'C:/Users/YIHANG/PycharmProjects/HTAD/dataset/videos/000049.mp4' ,0x7634706d, 20, size)
+# 得到视频size
+def get_size(img):
+    size = (img.shape[1], img.shape[0])
+    return size
 
-# 读取所有img存入列表
-img_array=[]
-for root, dirs, files in os.walk(r'C:\Users\YIHANG\PycharmProjects\HTAD\dataset\video_frames\000049'):
-    for f in files:
-        img = cv2.imread(root + "/" + f)
-        if img is None:
-            print(files + " is error!")
-            continue
-        img_array.append(img)
+# 得到该文件夹下帧的数量
+def get_num(folder):
+    count = 0
+    for file in folder:
+        ext = os.path.splitext(file)[-1].lower()
+        if ext == '.jpg':
+            count += 1
+    return count
 
-# 将读取的img存储为视频
-for i in range(52):
-    videowrite.write(img_array[i])
-videowrite.release()
-print('end!')
+# 将一个文件夹下所有帧存入列表, 并得到size
+def get_frames(path):
+    frames_array = []
+    for root, dirs, files in os.walk(path):
+        for f in files:
+            img = cv2.imread(root + "/" + f)
+            frames_array.append(img)
+    return frames_array
+
+# 制作视频
+def get_videos(video_path, size, count, array):
+    videos = cv2.VideoWriter(video_path, 0x7634706d, 25, size)
+    for i in range(count):
+        videos.write(array[i])
+    videos.release()
+    print("make " + video_path[52:62] + " successfully！")
+    return videos
+
+video_frames = []
+frames_count = []
+
+for root, dirs, files in os.walk(video_frames_path, topdown=True):
+    for j in dirs:
+        video_frames.append(root + j + "/")
+    frames_count.append(get_num(files))
+
+for idx, fra in enumerate(video_frames):
+    frames_array = get_frames(fra)
+    get_videos(fra[:45] + "videos" + fra[57:-1] + ".mp4", get_size(frames_array[0]), len(frames_array), frames_array)
+
+
+
+# videowrite = cv2.VideoWriter(r'C:/Users/YIHANG/PycharmProjects/HTAD/dataset/videos/000049.mp4' ,0x7634706d, 20, size)
