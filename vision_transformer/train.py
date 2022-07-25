9ca7ef65400fe3ab999aacc7376768740b0c4e2d
z520520115 <z520520115@gmail.com>
2022-07-25 16:13:51 +0900

    update

--- vision_transformer/train.py
+++ vision_transformer/train.py
@@ -115,7 +115,7 @@ def main(args):
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('--num_classes', type=int, default=2)
-    parser.add_argument('--epochs', type=int, default=500)
+    parser.add_argument('--epochs', type=int, default=200)
     parser.add_argument('--batch-size', type=int, default=32)
     parser.add_argument('--lr', type=float, default=0.001)
     parser.add_argument('--lrf', type=float, default=0.01)
